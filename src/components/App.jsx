c87e8caf4dd5260d8d79c0b56d1afb3836dac25d
d3monw1ngs <tezzielumsit87@gmail.com>
2024-07-05 23:39:00 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
+import React from 'react';
 import { Routes, Route} from 'react-router-dom';
-import { HomePage } from 'pages/Home/HomePage';
-import { NotFoundPage } from 'pages/NotFoundPage';
 import { SharedLayout } from 'pages/Shared/SharedLayout';
+import { HomePage } from 'pages/Home/HomePage';
+import { MoviesPage } from 'pages/Movies/MoviesPage';
+import { MovieDetailsPage } from 'pages/Movie/MovieDetailsPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 
 export const App = () => {
   return (
     <div>
+      <SharedLayout>
         <Routes>
-          <Route path="/" element={<SharedLayout />} />
           <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
+      </SharedLayout>
     </div>
   );
 };
